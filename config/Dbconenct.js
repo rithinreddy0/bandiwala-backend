06556b0f9aa8b6adb00c93e6a79f53d0a4a7bfc9
rithinreddy0 <135361821+rithinreddy0@users.noreply.github.com>
2024-10-22 20:03:47 +0530

    Update Dbconenct.js

--- config/Dbconenct.js
+++ config/Dbconenct.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const mongoose = require("mongoose")
  const db_connect = ()=>{
-    mongoose.connect(process.env.DBURL,{}).then(()=>{
+    mongoose.connect("mongodb+srv://bandiwala:bandiwala@1.8lkz9.mongodb.net/?retryWrites=true&w=majority&appName=1",{}).then(()=>{
         console.log("Data base connected successfully");
     }).catch((e)=>{
        console.log(process.env.DBURL);
