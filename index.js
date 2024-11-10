a6f72d7c53f1c0937884ebd449cd5fcb0ac677dc
navadeep11 <gujjulasainavadeepreddy@gmail.com>
2024-11-10 11:17:52 +0530

    cors

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const app = express();
 //middle wares
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: true }))
-app.use(cors());
+app.use(cors({origin:"*",methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], allowedHeaders: ['Content-Type', 'Authorization'] }));
 app.use(express.json());
 
 app.use(cookieparser());
